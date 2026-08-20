3bbb3c40248179491953ac2f1c5c2809bd674a61
test <test@example.com>
2026-08-20 19:54:15 +0800

    Day 1: first Python program

---
+++ main.py
@@ -0,0 +1,10 @@
+print("===AI English Learning Tool===")
+
+text = input("Enter a sentence in English: ")
+
+print()
+print("You sentence is:")
+print(text)
+
+print()
+print("Vocabulary analysis will be added soon.")
