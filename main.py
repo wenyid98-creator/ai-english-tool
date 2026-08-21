b70653907b4b72cf31a9a4617094354e66b915ae
test <test@example.com>
2026-08-21 18:02:36 +0800

    English text Analyzer V0.3

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-print("---ILL BE SOMEBODY IN FUTURE---")
-text = input("please enter your sentence: ")
+print("English text Analyzer V0.3")
+
+text = input("please enter your sentence:")
+
 words = text.split()
 
-for word in words:
-    print("" + word)
-    print("-")
+print("Total words:",len(words))
 
-print("Your total words are: ", len(words))
+print()
+
+print("Long words:")
+long_words=0
+for word in words:
+    if len(word) > 5:
+        long_words = long_words + 1
+        print("-",word)
+print("Number of long words",long_words)
+if long_words>=3:
+    print("This is a vocabulary-rich sentence.")
+else:
+    print("This sentence is relatively simple.")
