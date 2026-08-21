29bf2bbd974694b2b40ebc2a38b00bb0a0dba99b
test <test@example.com>
2026-08-21 17:06:32 +0800

    v0.2

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-print("=== AI English Learning Tool V0.2 ===")
-
-text = input("Enter an English sentence: ")
-
+print("---ILL BE SOMEBODY IN FUTURE---")
+text = input("please enter your sentence: ")
 words = text.split()
 
-print()
-print("Your words are:")
-
 for word in words:
-    print("-", word)
+    print("" + word)
+    print("-")
 
-print()
-print("Total words:", len(words))
+print("Your total words are: ", len(words))
